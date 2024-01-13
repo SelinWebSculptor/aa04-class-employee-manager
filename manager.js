ebe701b97b6d97d40d8df8ad0c8829924249c0e5
Anthony Botha <anthony.botha@outlook.com>
2024-01-13 11:26:06 -0800

    Phase 4

--- manager.js
+++ manager.js
@@ -1,7 +1,7 @@
 const Employee = require("./employee");
 
 class Manager extends Employee {
-    constructor(name, salary, title, manager, employees) {
+    constructor(name, salary, title, manager) {
         super(name, salary, title, manager)
         this.employees = []
     }
@@ -9,6 +9,23 @@ class Manager extends Employee {
     addEmployee(employee) {
         this.employees.push(employee)
     }
+
+    totalSubSalary(){
+        let sum = 0;
+
+        if (this.employees.length === 0){
+            return sum;
+        }
+
+        if (this.employee instanceof Manager){
+            sum = this.employee.calculateBonus() + this.salary;
+            return sum;
+        }
+
+        return totalSubSalary(this.employees.slice(0));
+    }
+
+
 }
 
 
