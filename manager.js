c828cf07a2b19e260b849ef3d2c0dfa61b2f7da8
Selin Uzunoglu <80995736+SelinWebSculptor@users.noreply.github.com>
2024-01-13 14:47:05 -0600

    completed

--- manager.js
+++ manager.js
@@ -9,20 +9,26 @@ class Manager extends Employee {
     addEmployee(employee) {
         this.employees.push(employee)
     }
+    calculateBonus(multiplier) {
+       return (this.salary + this._totalSubSalary()) * multiplier;
 
-    totalSubSalary(){
+    }
+    _totalSubSalary(){
         let sum = 0;
 
         if (this.employees.length === 0){
             return sum;
         }
 
-        if (this.employee instanceof Manager){
-            sum = this.employee.calculateBonus() + this.salary;
-            return sum;
+        for (let employee of this.employees) {
+            sum += employee.salary;
+        if (employee instanceof Manager) {
+            sum += employee._totalSubSalary();
+         }
         }
+        return sum;
 
-        return totalSubSalary(this.employees.slice(0));
+
     }
 
 
